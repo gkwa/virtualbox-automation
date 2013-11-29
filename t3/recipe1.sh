bf9b6e0dee00f3edb2e529921cc24f940ddcf560
taylor <tailor@u.washington.edu>
2013-11-29 14:36:26 -0800

    Add virtualbox recipe
    Change-Id: I10be18f86bfe0d8e364ad8b69a0d1a17648c207b

---
+++ t3/recipe1.sh
@@ -0,0 +1,55 @@
+set -o errexit
+set -o nounset
+
+MY_DIR=`dirname $0`
+source $MY_DIR/recipe1_settings.sh
+
+set +o errexit #when vmname doesn't exist
+VBoxManage controlvm "$vmname" poweroff 2>/dev/null
+VBoxManage unregistervm "$vmname" --delete 2>/dev/null
+set -o errexit
+
+VBoxManage createvm --name "$vmname" --ostype $ostype --register
+VBoxManage createhd --filename "$vmbasedir/$vmname.vdi" --size $hdd_size --format VDI
+VBoxManage modifyvm "$vmname" --memory 1286
+VBoxManage modifyvm "$vmname" --acpi on
+VBoxManage modifyvm "$vmname" --usb on
+VBoxManage modifyvm "$vmname" --mouse usbtablet
+VBoxManage modifyvm "$vmname" --boot4 none
+VBoxManage modifyvm "$vmname" --boot3 none
+VBoxManage modifyvm "$vmname" --boot2 dvd
+VBoxManage modifyvm "$vmname" --boot1 disk
+VBoxManage modifyvm "$vmname" --clipboard bidirectional
+VBoxManage modifyvm "$vmname" --draganddrop hosttoguest
+VBoxManage modifyvm "$vmname" --nic1 bridged --bridgeadapter1 "$nic1_bridged_adapter"
+
+# I need this on.  It took me a long time to disover this PAE should be on
+VBoxManage modifyvm "$vmname" --pae on
+
+
+# #############################
+# IDE Controller
+# #############################
+VBoxManage storagectl "$vmname" --name IDE --add ide
+
+if test ! -z "$iso1"
+then
+    VBoxManage storageattach "$vmname" --storagectl IDE \
+	--port 0 --device 0 --type dvddrive --medium "$iso1"
+fi
+
+#VBoxManage storageattach "$vmname" --storagectl IDE \
+#    --port 1 --device 0 --type dvddrive --medium emptydrive
+
+# #############################
+# Sata Controller
+# #############################
+VBoxManage storagectl "$vmname" --name "SATA Controller" --add sata
+VBoxManage storageattach "$vmname" --storagectl "SATA Controller" \
+    --port 0 --device 0 --type hdd --medium "$vmbasedir/$vmname.vdi"
+
+if test ! -z "$iso2"
+then
+    VBoxManage storageattach "$vmname" --storagectl "SATA Controller" \
+	--port 1 --device 0 --type dvddrive --medium "$iso2"
+fi
---
+++ t3/recipe1_settings.sh
@@ -0,0 +1,8 @@
+vmname=w71
+nic1_bridged_adapter="Intel(R) 82579V Gigabit Network Connection"
+
+ostype=Windows7
+vmbasedir=d:/vbox/$vmname
+hdd_size=$((200*1000)) #200GB
+iso1=c:/cygwin/home/streambox/pdev/windows-sim/w7pro.iso
+iso2=d:/wsusoffline86_1/wsusoffline/iso/wsusoffline-w61.iso
