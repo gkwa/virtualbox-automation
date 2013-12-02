aa1a31eb3b61a5e6cf071c0ecf921fdf7ab8ac13
taylor <tailor@u.washington.edu>
2013-12-01 20:01:34 -0800

    t
    Change-Id: I3edb7b3d1e34d8b3909c09b42fbfd98c46bbe069

--- t3/common.sh
+++ t3/common.sh
@@ -1,5 +1,15 @@
 delete_vm()
 {
+    vboxmanage list runningvms |
+    cut -d\" -f2 |
+    while read vm
+    do
+	if test "z$vm" = "z$vmname"
+	then
+	    VBoxManage controlvm "$vm" poweroff
+	fi
+    done
+
     vboxmanage list vms |
     cut -d\" -f2 |
     while read vm
@@ -16,14 +26,4 @@ delete_vm()
 	VBoxManage -q closemedium disk "$hdd_abspath" --delete
 	rm -f "$hdd_abspath"
     fi
-
-    vboxmanage list runningvms |
-    cut -d\" -f2 |
-    while read vm
-    do
-	if test "z$vm" = "z$vmname"
-	then
-	    VBoxManage controlvm "$vm" poweroff
-	fi
-    done
 }
