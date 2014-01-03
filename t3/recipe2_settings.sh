e21ecaf5b1b391809f65f0507e40166c89cda5f9
taylor <tailor@u.washington.edu>
2014-01-02 22:47:53 -0800

    Hard code vm name to be similar to the recipe.sh script
    Change-Id: I5066ce5ce9d3f9082bc9eba2dab7bb2492336e72

--- t3/recipe2_settings.sh
+++ t3/recipe2_settings.sh
@@ -2,7 +2,7 @@ set -o errexit
 set -o nounset
 
 
-vmname=t12
+vmname=$(echo $0 | sed -e 's,_settings,,' -e 's,\.sh,,')
 nic1_bridged_adapter="Intel(R) 82579V Gigabit Network Connection"
 
 basedir=$(pwd)
