5909360d02f1a3569848b9ba4da924b2aab60ad2
taylor <tailor@u.washington.edu>
2014-03-18 22:40:42 -0700

    Add recipe8 to get ISO from c:/MDTDSTest* dir
    Change-Id: I17e3190cee1d1c62c8651ba9a953c429fdaa78f8

---
+++ t3/recipe8.sh
@@ -0,0 +1,79 @@
+set -o errexit
+set -o nounset
+
+MY_DIR=`dirname $0`
+source $MY_DIR/recipe8_settings.sh
+source $MY_DIR/common.sh
+
+delete_vm "$vmname"
+VBoxManage createhd --filename "$hdd_abspath" --size $hdd_size --format VDI
+VBoxManage createvm --name "$vmname" --ostype $ostype --register
+VBoxManage modifyvm "$vmname" --vram 80 # Fixes warning "Non-Optimal settings detected", 20 MB ram, set more for windows of course
+VBoxManage modifyvm "$vmname" --memory 1500
+VBoxManage modifyvm "$vmname" --acpi on
+VBoxManage modifyvm "$vmname" --usb on
+VBoxManage modifyvm "$vmname" --mouse usb
+VBoxManage modifyvm "$vmname" --boot4 none
+VBoxManage modifyvm "$vmname" --boot3 none
+VBoxManage modifyvm "$vmname" --boot2 dvd
+VBoxManage modifyvm "$vmname" --boot1 disk
+VBoxManage modifyvm "$vmname" --clipboard bidirectional
+VBoxManage modifyvm "$vmname" --draganddrop hosttoguest
+VBoxManage modifyvm "$vmname" --ioapic on
+VBoxManage modifyvm "$vmname" --pae on # I need this on.  It took me a long time to disover this PAE should be on
+VBoxManage modifyvm "$vmname" --nic1 bridged \
+    --bridgeadapter1 "$nic1_bridged_adapter" --nictype1 virtio
+VBoxManage modifyvm "$vmname" --vcpenabled on
+
+# #############################
+# IDE Controller
+# #############################
+VBoxManage storagectl "$vmname" --name IDE --add ide
+
+if test ! -z "$iso1"
+then
+    VBoxManage storageattach "$vmname" --storagectl IDE \
+    	--port 0 --device 0 --type hdd --medium "$vmbasedir/$vmname.vdi"
+fi
+
+# #############################
+# Sata Controller
+# #############################
+VBoxManage storagectl "$vmname" --name "SATA Controller" --add sata --portcount 3
+VBoxManage storageattach "$vmname" --storagectl "SATA Controller" \
+	--port 0 --device 0 --type dvddrive --medium "$iso1"
+
+
+
+
+
+
+
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if test ! -z "$iso2"
+then
+    VBoxManage storageattach "$vmname" --storagectl "SATA Controller" \
+	--port 1 --device 0 --type dvddrive --medium "$iso2"
+fi
+
+if test ! -z "$iso3"
+then
+    VBoxManage storageattach "$vmname" --storagectl "SATA Controller" \
+	--port 2 --device 0 --type dvddrive --medium "$iso3"
+fi
---
+++ t3/recipe8_settings.sh
@@ -0,0 +1,26 @@
+set -o errexit
+set -o nounset
+
+vmname=$(echo $0 | sed -e 's,_settings,,' -e 's,\.sh,,')
+nic1_bridged_adapter="Intel(R) 82579V Gigabit Network Connection"
+
+basedir=$(pwd)
+if test "z$(uname -s | grep -i cygwin)" != z
+then
+    basedir=$(cygpath --mixed -w "$(pwd)")
+fi
+
+virtio_net_version=0.1-74
+virtio_net_iso=virtio-win-${virtio_net_version}.iso
+virtio_net_iso_abspath=$basedir/$virtio_net_iso
+
+wget -nv --timestamping http://alt.fedoraproject.org/pub/alt/virtio-win/latest/images/$virtio_net_iso
+
+ostype=Windows7
+vmbasedir=d:/vbox/$vmname
+hdd="$vmname.vdi"
+hdd_abspath=$vmbasedir/$hdd
+hdd_size=$((25*1000)) #GB
+iso1=$(ls -1dt /c/MDTDStest* | head -1 | xargs -I@ find @ -iname "*.iso" | xargs cygpath --mixed)
+iso2=c:/Program\ Files/Oracle/VirtualBox/VBoxGuestAdditions.iso
+iso3=$virtio_net_iso_abspath
